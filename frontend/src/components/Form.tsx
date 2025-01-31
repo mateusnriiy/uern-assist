39993dcaf8e72d7da3054a8dac757b7eb510ca73
mateusnriiy <mateusgomesneri@gmail.com>
2025-01-31 15:34:02 -0300

    style: Adição do formulário e estilização

---
+++ frontend/src/components/Form.tsx
@@ -0,0 +1,27 @@
+import '../css/index.css'
+
+function Form() {
+  return (
+    <div className="area-content">
+      {/* <div className='logo-sistena'>
+        <img src="../../assets/1.png" alt="" />
+      </div> */}
+      <form className='form-content'>
+        <p>Nome</p>
+        <input placeholder='Ex: João Maria' className="input input-name" type="text" name="nome"/>
+        
+        <p>Computador</p>
+        <input placeholder='Ex: CAN-01' className="input input-computer" type="text" name="computador"/>
+
+        <p>Informe o problema</p>
+        <textarea placeholder='Informe...' className="input input-problem" name="problema"></textarea>
+
+        <button className='btn-submit' type='submit'>
+          Enviar
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default Form;
