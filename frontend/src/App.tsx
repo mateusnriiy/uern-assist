31fc3d1230c555415a5ab12fc3ffc7cd015e45db
mateusnriiy <mateusgomesneri@gmail.com>
2025-02-01 00:23:28 -0300

    style: Adição das logos e estilização do button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,8 +5,15 @@ function App() {
   return (
     <main>
       <div className='layout'>
-        <div className='layout-logo'></div>
+        <div className='layout-logo'>
+          <div className='logo-layoutdti'>
+          <img src="../assets/Logo-UernDTI.png" alt="Logo do DTI" className='logo-dti' />
+          </div>
+        </div>
         <div className='layout-form'>
+          <div className='logo-layout'>
+          <img src="../assets/Logo-UernAssist.png" alt="Logo do Sistema" className='logo-sistem'/>
+          </div>
           <Form />
         </div>
       </div>
--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -3,22 +3,22 @@ import '../css/index.css'
 function Form() {
   return (
     <div className="area-content">
-      {/* <div className='logo-sistena'>
-        <img src="../../assets/1.png" alt="" />
-      </div> */}
       <form className='form-content'>
         <p>Nome</p>
-        <input placeholder='Ex: João Maria' className="input input-name" type="text" name="nome"/>
-        
+        <input placeholder='Ex: João Maria' className="input input-name" type="text" name="nome" />
+
         <p>Computador</p>
-        <input placeholder='Ex: CAN-01' className="input input-computer" type="text" name="computador"/>
+        <input placeholder='Ex: CAN-01' className="input input-computer" type="text" name="computador" />
 
         <p>Informe o problema</p>
         <textarea placeholder='Informe...' className="input input-problem" name="problema"></textarea>
 
-        <button className='btn-submit' type='submit'>
-          Enviar
-        </button>
+        <div className='btn-container'>
+          <button className='btn-submit' type='submit'>
+            Enviar
+          </button>
+        </div>
+
       </form>
     </div>
   )
