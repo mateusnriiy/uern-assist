4d0feb2b65f609c74cae7f4f68f6ef451f315419
mateusnriiy <mateusgomesneri@gmail.com>
2025-02-03 10:05:19 -0300

    style: Modificação do botão e da logo principal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import Form from './components/Form';
+import Form from './components/Form.tsx';
 import './css/index.css';
 
 function App() {
@@ -7,14 +7,19 @@ function App() {
       <div className='layout'>
         <div className='layout-logo'>
           <div className='logo-layoutdti'>
-          <img src="../assets/Logo-UernDTI.png" alt="Logo do DTI" className='logo-dti' />
+            <img src="../assets/Logo-UernDTI.png" alt="Logo do DTI" className='logo-dti' />
           </div>
         </div>
         <div className='layout-form'>
           <div className='logo-layout'>
-          <img src="../assets/Logo-UernAssist.png" alt="Logo do Sistema" className='logo-sistem'/>
+            <img src="../assets/Logo-UernAssist.png" alt="Logo do Sistema" className='logo-sistem' />
           </div>
           <Form />
+          <div className='btn-container'>
+            <button className='btn-submit' type='submit'>
+              Enviar
+            </button>
+          </div>
         </div>
       </div>
     </main>
--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -3,7 +3,7 @@ import '../css/index.css'
 function Form() {
   return (
     <div className="area-content">
-      <form className='form-content'>
+      <form action='' className='form-content'>
         <p>Nome</p>
         <input placeholder='Ex: João Maria' className="input input-name" type="text" name="nome" />
 
@@ -12,13 +12,7 @@ function Form() {
 
         <p>Informe o problema</p>
         <textarea placeholder='Informe...' className="input input-problem" name="problema"></textarea>
-
-        <div className='btn-container'>
-          <button className='btn-submit' type='submit'>
-            Enviar
-          </button>
-        </div>
-
+          
       </form>
     </div>
   )
