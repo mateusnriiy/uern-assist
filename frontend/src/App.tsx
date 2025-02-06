53a91f70d5567b80f4946fd88abd00bab6cc6c3a
Lucas Lima <lucasbezerra14.ll@gmail.com>
2025-02-06 20:46:22 -0300

    feat: Construção da lógica do frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,23 @@ import Button from './components/Button.tsx';
 import Form from './components/Form.tsx';
 import Header from './components/Header.tsx';
 import SystemLogo from './components/SystemLogo.tsx';
+import { useState } from 'react';
+import { FormDados } from './interfaces/FormInterface.ts';
 
 function App() {
+  const [formDados, setFormDados] = useState<FormDados>({
+    nome: '',
+    pcId: '',
+    feed: '',
+  })
+
   return (
     <main>
       <div className='layout'>
         <Header />
         <div className='layout-form'>
           <SystemLogo />
-          <Form />
+          <Form setFormDados={setFormDados} />
           <Button />
         </div>
       </div>
--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -1,19 +1,59 @@
+import React, { useState } from 'react';
 import '../css/index.css';
+import { FormProps } from '../interfaces/FormInterface';
 
-function Form() {
+function Form({ setFormDados }: FormProps) {
+  const [nome, setNome] = useState<string>('');
+  const [pcId, setPcId] = useState<string>('');
+  const [feed, setFeed] = useState<string>('');
+  const [errors, setErrors] = useState<{ nome: boolean; pcId: boolean; feed: boolean }>({ nome: false, pcId: false, feed: false });
+
+  const pcs = Array.from({ length: 28 }, (_, i) => `CAM-${(i + 1).toString().padStart(2, '0')}`);
+
+  const handleValidation = () => {
+    setErrors({
+      nome: nome.length < 5,
+      pcId: false,
+      feed: feed.length < 10,
+    })
+  }
+
+  const handleChange = (setter: React.Dispatch<React.SetStateAction<string>>, value: string) => {
+    setter(value);
+    handleValidation();
+    setFormDados((prev) => ({ ...prev, [setter === setNome ? 'nome' : setter === setPcId ? 'pcId' : 'feed']: value }));
+  }
+
   return (
     <div className="area-content">
       <form action='' className='form-content'>
         <label>Nome</label>
-        <input placeholder='Ex: João Maria' className="input input-name" type="text" name="nome" />
+        <input placeholder='Ex: João Maria'
+          value={nome}
+          onChange={(e) => handleChange(setNome, e.target.value)}
+          onBlur={handleValidation}
+          className={`input input-name ${errors.nome ? 'msg-error' : ''}`}
+          type="text" />
 
         <label>Computador</label>
-        <input placeholder='Ex: CAN-01' className="input input-computer" type="text" name="computador" />
+        <div>
+          <select value={pcId} onChange={(e) => handleChange(setPcId, e.target.value)}>
+            <option value="">Selecione uma máquina</option>
+            {pcs.map((pc) => (
+              <option key={pc} value={pc}>{pc}</option>
+            ))}
+          </select>
+        </div>
+        {/* <input placeholder='Ex: CAN-01'
+          className={`input input-computer`}
+          type="text" /> */}
 
         <label>Informe o problema</label>
         <textarea placeholder='Informe...'
-          className="input input-problem"
-          name="problema"
+          value={feed}
+          onChange={(e) => handleChange(setFeed, e.target.value)}
+          onBlur={handleValidation}
+          className={`input input-name ${errors.feed ? 'msg-error' : ''}`}
           rows={8}>
         </textarea>
       </form>
