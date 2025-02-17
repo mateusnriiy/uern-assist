3815e861e6c69627093974c5ca885bfb2faf4923
Lucas Lima <lucasbezerra14.ll@gmail.com>
2025-02-17 15:47:03 -0300

    feat: criação do modal e outras alterações no front

--- frontend/src/interfaces/ButtonInterface.ts
+++ frontend/src/interfaces/ButtonInterface.ts
@@ -2,5 +2,6 @@ import { FormDados } from './FormInterface';
 
 export interface ButtonProps {
   dados: FormDados,
-  setErrors: React.Dispatch<React.SetStateAction<{ nome: boolean; pcId: boolean; feed: boolean }>>;
+  setErrors: React.Dispatch<React.SetStateAction<{ nome: boolean; pcId: boolean; feed: boolean }>>,
+  onSuccess: () => void;
 }
--- frontend/src/interfaces/FormInterface.ts
+++ frontend/src/interfaces/FormInterface.ts
@@ -5,6 +5,7 @@ export interface FormDados {
 }
 
 export interface FormProps {
+  formDados: FormDados;
   setFormDados: React.Dispatch<React.SetStateAction<FormDados>>,
   errors: { nome: boolean; pcId: boolean; feed: boolean },
   setErrors: React.Dispatch<React.SetStateAction<{ nome: boolean; pcId: boolean; feed: boolean }>>;
