7d9cafd5e73f39986f6ea82c5400a66d4dfe5821
Lucas Lima <lucasbezerra14.ll@gmail.com>
2025-02-25 15:19:25 -0300

    feat: ajustes da model e dbcontext do projeto

--- backend/Data/AppDbContext.cs
+++ backend/Data/AppDbContext.cs
@@ -6,7 +6,7 @@ namespace backend.Data
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-        public DbSet<Solicitacao> Solicitacaos { get; set; }
+        public DbSet<Solicitacoes> Solicitacoes { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
--- backend/Models/Solicitacoes.cs
+++ backend/Models/Solicitacoes.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace backend.models
 {
-    public class Solicitacao
+    public class Solicitacoes
     {
         [Key]
         public int Id { get; set; }
